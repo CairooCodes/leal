0ea68656753d95c98eda7f94f7722f7737121af0
WallaceNeryVeras <wallacenery.wn@gmail.com>
2023-10-02 19:40:50 -0300

    add video and working who we are

--- components/quem-somos.php
+++ components/quem-somos.php
@@ -5,8 +5,17 @@
             <div class="flex justify-center items-center">
                 <div class="mt-16 w-3/4">
                     <p class="text-color1 text-justify">
-                        Novos tempos, novos desafios, requerem novas atitudes, mudanças que precisam ser feitas para um ajuste às demandas do mundo atual, tanto na vida pessoal como na profissional. Para fazer frente a esses novos desafios que surgem a cada dia, empresas estão se adaptando e buscando ajuda para continuarem atuantes e crescendo. Com esta decisão, é importante ir em busca da <span class="font-bold text-color1">Melhor Agência de Marketing Digital em Teresina</span>, que saiba como dar o destaque que sua empresa precisa no cenário digital.
-                    </p>
+                        São 12 anos de história!!!
+                        Durante a trajetória, o equilíbrio entre as dualidades da vida!!
+                        Passos firmes, pausas uma vez ou outra! Desistir, jamais!!!!
+                        <br>
+                        Assim nasce a Leal Comunicação, uma empresa focada no propósito da lealdade, da dignidade e de amar comunicar!✨
+                        <br>
+                        Tudo em nós pulsa🎯
+                        <br>
+                        Criatividade. Alegria. Amor. Dedicação. Comunicação.
+                        <br>
+                        Apresentamos a vocês, nossa nova marca!!!✨💜 captada pela nossa essência e conectada ao nosso propósito!!!</p>
                     <br>
                     <p class="text-color1 text-justify">
                         Só com a Leal Comunicação & Marketing, a <span class="font-bold text-color1">Melhor Agência de Marketing Digital em Teresina</span> você tem acesso aos melhores serviços e soluções do marketing digital, para poder ajudar com todos os objetivos que você tem para sua empresa, sejam eles de crescimento, expansão ou divulgação.
@@ -15,25 +24,6 @@
                 <div class="w-full hidden">
                     <img src="./assets/img/Marketing-Digital.png" alt="Marketing-Digital" class="flex items-center justify-center w-full">
                 </div>
-            </div>
-        </div>
-    </div>
-    <div class="bg-color1">
-        <div class="max-w-screen-xl mx-auto lg:px-6">
-            <div class="flex justify-center items-center">
-                <h1 class="m-5 font-bold text-3xl text-center text-white">
-                    Está afim de ter seu espaço na internet?
-                    <br>
-                    Fale com a gente!
-                </h1>
-                <a href="#">
-                    <button class="flex items-center my-2 w-full cursor-pointer rounded-md bg-color2 py-3 px-6 font-bold text-color1 text-lg duration-300 ease-in-out hover:bg-color4 hover:text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
-                            <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
-                        </svg>
-                        <input type="submit" value="Chama a gente!" class="ml-2">
-                    </button>
-                </a>
             </div>
         </div>
     </div>

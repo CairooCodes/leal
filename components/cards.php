e291a547ec01aa18cb32e7650e9b9537c7052798
Cairo <cairofelipedev@gmail.com>
2023-10-27 16:07:56 -0300

    .gitignore is now working

---
+++ components/cards.php
@@ -0,0 +1,49 @@
+<div class="flex justify-center space-x-10 mt-16 mb-16">
+    <div class="p-6 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            Marketing Digital
+        </h5>
+        <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+            O marketing digital envolve estratégias online para aumentar a visibilidade da marca, atrair tráfego qualificado, envolver clientes e impulsionar vendas.
+            É essencial na era digital, permitindo que empresas alcancem um público global de forma eficaz e mensurável, aproveitando o aumento do uso da internet.
+        </p>
+        <button class="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button" data-ripple-light="true">
+            Read More
+        </button>
+    </div>
+
+    <div class="p-6 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            Tráfego pago
+        </h5>
+        <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+            O tráfego pago é uma estratégia de marketing que direciona visitantes para um site por meio de anúncios pagos em plataformas online como Google Ads e Facebook Ads.
+            Anunciantes pagam por cliques, visualizações ou ações específicas. É rápido e eficaz, aumentando a visibilidade, gerando leads qualificados e impulsionando as vendas, sendo essencial no marketing digital.
+        </p>
+        <button class="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button" data-ripple-light="true">
+            Read More
+        </button>
+    </div>
+
+    <div class="p-6 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+        <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+            Ações de fortalecimento de marca
+        </h5>
+        <p class="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+            Fortalecimento de marca envolve consistência na identidade visual, mensagens claras e valores autênticos.
+            Empresas investem em experiências excepcionais, feedbacks positivos e engajamento social para construir reputação sólida.
+            Uso consistente de elementos visuais e comunicação transparente contribuem para confiança do consumidor e lealdade à marca.
+        </p>
+        <button class="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" type="button" data-ripple-light="true">
+            Read More
+        </button>
+    </div>
+</div>
+
+<!-- stylesheet -->
+<link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css" />
+
+<!-- Ripple Effect -->
+
+<!-- from cdn -->
+<script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>

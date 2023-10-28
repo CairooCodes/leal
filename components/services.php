33889cca600ef5b6fccfc37ae8d9dacf73e5c512
AleexBrendon <alexbrendo.dev@gmail.com>
2023-10-27 21:48:24 -0300

    ajusts services leal

--- components/services.php
+++ components/services.php
@@ -1,43 +1,48 @@
-<div class="flex justify-center space-x-10 mt-16 mb-16" id="services">
-  <div class="p-6 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-    <h5 class="mb-2 block font-sans text-xl text-center font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-      Marketing Digital
-    </h5>
-    <p class="block font-sans text-base text-center font-light leading-relaxed text-inherit antialiased mb-8 mt-2">
-      O marketing digital aumenta a visibilidade, atrai tráfego qualificado e impulsiona vendas online, essencial na era digital. Permitindo alcance à um público global de forma eficaz e mensurável.
-    </p>
-    <a href="https://api.whatsapp.com/send/?phone=5586998505368&text&type=phone_number&app_absent=0" class="select-none mt-auto mx-auto rounded-lg bg-color1 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-color2 transition-all hover:shadow-lg hover:shadow-color2 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-      <button type="button" data-ripple-light="true">
-        WhatsApp <i class="ml-2 bi bi-whatsapp"></i>
-      </button>
-    </a>
-  </div>
+<div class="flex justify-center space-x-10 lg:py-16 md:p-8 lg:p-10" id="services">
+  <div class="grid grid-cols-1 lg:grid-cols-3">
+    <div class="p-6 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <img class="mb-5 rounded" src="./assets/img/Marketing-Digital.png" alt="Marketing-Digital">
+      <h5 class="mb-2 block font-sans text-xl text-center font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+        Marketing Digital
+      </h5>
+      <p class="block font-sans text-base text-center font-light leading-relaxed text-inherit antialiased mb-8 mt-2">
+        O marketing digital aumenta a visibilidade, atrai tráfego qualificado e impulsiona vendas online, essencial na era digital. Permitindo alcance à um público global de forma eficaz e mensurável.
+      </p>
+      <a href="https://api.whatsapp.com/send/?phone=5586998505368&text&type=phone_number&app_absent=0" class="select-none mt-auto mx-auto rounded-lg bg-color1 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-color2 transition-all hover:shadow-lg hover:shadow-color2 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+        <button type="button" data-ripple-light="true">
+          WhatsApp <i class="ml-2 bi bi-whatsapp"></i>
+        </button>
+      </a>
+    </div>
 
-  <div class="p-6 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-    <h5 class="mb-2 block font-sans text-xl text-center font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-      Tráfego pago
-    </h5>
-    <p class="block font-sans text-base text-center font-light leading-relaxed text-inherit antialiased mb-8 mt-2">
-      Tráfego pago: anúncios direcionam visitantes via Google Ads e Facebook Ads, gerando leads e impulsionando vendas no marketing digital.
-    </p>
-    <a href="https://api.whatsapp.com/send/?phone=5586998505368&text&type=phone_number&app_absent=0" class="select-none mt-auto mx-auto rounded-lg bg-color1 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-color2 transition-all hover:shadow-lg hover:shadow-color2 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-      <button type="button" data-ripple-light="true">
-        WhatsApp <i class="ml-2 bi bi-whatsapp"></i>
-      </button>
-    </a>
-  </div>
+    <div class="p-6 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <img class="mb-5 rounded" src="./assets/img/Trafego-Pago.png" alt="Trafego-Pago">
+      <h5 class="mb-2 block font-sans text-xl text-center font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+        Tráfego pago
+      </h5>
+      <p class="block font-sans text-base text-center font-light leading-relaxed text-inherit antialiased mb-8 mt-2">
+        Tráfego pago: anúncios direcionam visitantes via Google Ads e Facebook Ads, gerando leads e impulsionando vendas no marketing digital.
+      </p>
+      <a href="https://api.whatsapp.com/send/?phone=5586998505368&text&type=phone_number&app_absent=0" class="select-none mt-auto mx-auto rounded-lg bg-color1 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-color2 transition-all hover:shadow-lg hover:shadow-color2 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+        <button type="button" data-ripple-light="true">
+          WhatsApp <i class="ml-2 bi bi-whatsapp"></i>
+        </button>
+      </a>
+    </div>
 
-  <div class="p-6 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
-    <h5 class="mb-2 block font-sans text-xl text-center font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-      Ações de fortalecimento de marca
-    </h5>
-    <p class="block font-sans text-base text-center font-light leading-relaxed text-inherit antialiased mb-8 mt-2">
-      Fortalecimento de marca: consistência na identidade visual, experiências excepcionais e comunicação transparente geram confiança e lealdade dos consumidores.
-    </p>
-    <a href="https://api.whatsapp.com/send/?phone=5586998505368&text&type=phone_number&app_absent=0" class="select-none mt-auto mx-auto rounded-lg bg-color1 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-color2 transition-all hover:shadow-lg hover:shadow-color2 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-      <button type="button" data-ripple-light="true">
-        WhatsApp <i class="ml-2 bi bi-whatsapp"></i>
-      </button>
-    </a>
+    <div class="p-6 relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <img class="mb-5 rounded" src="./assets/img/acoes-de-fortalecimento-de-marca.png" alt="acoes-de-fortalecimento-de-marca">
+      <h5 class="mb-2 block font-sans text-xl text-center font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+        Ações de fortalecimento de marca
+      </h5>
+      <p class="block font-sans text-base text-center font-light leading-relaxed text-inherit antialiased mb-8 mt-2">
+        Fortalecimento de marca: consistência na identidade visual, experiências excepcionais e comunicação transparente geram confiança e lealdade dos consumidores.
+      </p>
+      <a href="https://api.whatsapp.com/send/?phone=5586998505368&text&type=phone_number&app_absent=0" class="select-none mt-auto mx-auto rounded-lg bg-color1 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-color2 transition-all hover:shadow-lg hover:shadow-color2 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+        <button type="button" data-ripple-light="true">
+          WhatsApp <i class="ml-2 bi bi-whatsapp"></i>
+        </button>
+      </a>
+    </div>
   </div>
 </div>

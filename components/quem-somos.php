d3f5856a0a1e1507dcfd0551f9b83cc5862a866a
AleexBrendon <alexbrendo.dev@gmail.com>
2024-01-03 12:01:41 -0300

    ajust text quem somos leal

--- components/quem-somos.php
+++ components/quem-somos.php
@@ -2,26 +2,19 @@
   <div class="max-w-screen-xl px-4 py-4 mx-auto lg:py-8 lg:px-8">
     <div>
       <h1 class="font-bold text-3xl text-color4 text-center px-2">MELHOR AGÊNCIA DE MARKETING DIGITAL EM TERESINA</h1>
-      <div class="mx-auto px-4">
-        <div class="text-center pt-10">
+      <div class="mx-auto px-8">
+        <div class="text-center pt-10 text-justify">
           <p class="text-color1">
             A LEAL COMUNICAÇÃO NASCEU EM 12.07.11. DURANTE A TRAJETÓRIA, O EQUILÍBRIO ENTRE AS DUALIDADES DA VIDA... PASSOS FIRMES, PAUSAS UMA VEZ OU OUTRA... DESISTIR, JAMAIS!!!!
-            <br>
             HOJE, TEMOS UM PROPÓSITO FINCADO NA LEALDADE, ÉTICA E NA DIGNIDADE DE COMUNICAR E AMAR!✨
-            <br>
             TUDO EM NÓS PULSA🎯
-            <br>
             CRIATIVIDADE. ALEGRIA. AMOR. DEDICAÇÃO. COMUNICAÇÃO.
-            <br>
             APRESENTAMOS A VOCÊS, NOSSO SITE!!! ✨💜 DAÍ VOCÊ CONSEGUE SENTIR NOSSA ESSÊNCIA? AINDA NÃO?! POIS... QUE TAL MARCAMOS UM CAFÉ AQUI NA AGÊNCIA? VOCÊS SERÃO MUITO BEM VINDOS!!!
           </p>
           <br>
-          <p class="text-color1">
-            <span class="font-bold text-color1">Só com a Leal Comunicação & Marketing</span>, a Melhor Agência de Marketing Digital em Teresina você tem acesso aos melhores serviços e soluções do marketing digital, para poder ajudar com todos os objetivos que você tem para sua empresa, sejam eles de crescimento, expansão ou divulgação.
+          <p class="text-color1 uppercase">
+            <span class="font-bold text-color1">Só com a Leal Comunicação & Marketing</span>, a Melhor Agência de Marketing Digital em Teresina você tem acesso aos melhores serviços e soluções do marketing digital, para poder ajudar com todos os objetivos que você tem para sua empresa, sejam eles de crescimento, expansão ou divulgação. Busca uma agência 360 graus? Pois estacione aqui conosco! Somos o que você procura!
 
-          </p>
-          <p class="text-color1">
-            Busca uma agência 360 graus? Pois estacione aqui conosco! Somos o que você procura!
           </p>
         </div>
         <div class="w-full hidden">
